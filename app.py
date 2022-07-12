5c7ed4b2d0dc58b7d50aa2d544d968eae49f09fc
phenomenonlee <flejdud8@naver.com>
2022-07-12 23:15:51 +0900

    1

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask, render_template, request, jsonify
+from pymongo import MongoClient
+from datetime import datetime, timedelta
+import hashlib
+import jwt
 import requests
+
 
 app = Flask(__name__)
 
 from pymongo import MongoClient
+
+SECRET_KEY = 'SPARTA'
+
 client = MongoClient('mongodb+srv://test:sparta@cluster0.tef1s.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
 
@@ -11,24 +19,24 @@ db = client.dbsparta
 # 라우터들
 @app.route("/")
 def main():
-
     return render_template('index.html')
 
-@app.route("/" , methods=["POST"])
+
+@app.route("/", methods=["POST"])
 def insert_contents_post():
     try:
         idx = request.form['index']
     except:
         idx = 0
-    artist_receive =request.form['artist_give']
+    artist_receive = request.form['artist_give']
     title_receive = request.form['title_give']
     desc_receive = request.form['desc_give']
     url_receive = request.form['url_give']
-    doc = {'index':idx,
-        'artist':artist_receive,
-           'title':title_receive,
-           'desc':desc_receive,
-           'url':url_receive }
+    doc = {'index': idx,
+           'artist': artist_receive,
+           'title': title_receive,
+           'desc': desc_receive,
+           'url': url_receive}
     db.contents.insert_one(doc)
 
     return jsonify({'msg': '데이터 POST'})
@@ -38,7 +46,7 @@ def insert_contents_post():
 def insert_contents_get():
     contents = list(db.contents.find({}, {'_id': False}))
 
-    return jsonify({'contents' : contents})
+    return jsonify({'contents': contents})
 
 
 @app.route('/login')
@@ -75,9 +83,51 @@ def footer():
 # 여기부터 기능들
 @app.route('/detail', methods=["POST"])
 def detail_post():
-
-
     return jsonify({'msg': '보내기 완료'})
+
+
+# login&signup
+
+@app.route('/sign_up/check_dup', methods=['POST'])
+def check_dup():
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"id": username_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
+
+
+@app.route('/sign_up/save', methods=['POST'])
+def sign_up():
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    doc = {
+        "id": username_receive,  # 아이디
+        "pw": password_hash,  # 비밀번호
+    }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
+
+
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    # 로그인
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'id': username_receive, 'pw': pw_hash})
+
+    if result is not None:
+        payload = {
+            'id': username_receive,
+            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
+
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
 
 if __name__ == '__main__':
