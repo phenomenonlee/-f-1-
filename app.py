7c4dbe915aadd70f0bd4b9de10db63007c769785
hanyang-jeil-gum <qhgusdl177@naver.com>
2022-07-12 10:06:37 +0900

    1주차 프로젝트 음악 추천 공유 웹사이트 Song & C 웹사이트 기본 뼈대

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    return render_template("index.html")
+
+
+@app.route('/mypage')
+def mypage():
+    return render_template("mypage.html")
+
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+
+@app.route('/signup')
+def signup():
+    return render_template("signup.html")
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
