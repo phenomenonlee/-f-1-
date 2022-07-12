220ef5a902a3ca173d2252a8316d9b20435d600f
bo hyeon kim <qhgusdl177@naver.com>
2022-07-12 20:31:46 +0900

    ripple 내역을 서버로 저장

--- app.py
+++ app.py
@@ -6,9 +6,6 @@ client = MongoClient('mongodb+srv://test:sparta@cluster0.tef1s.mongodb.net/?retr
 db = client.dbsparta
 
 
-
-
-
 @app.route('/')
 def index():
     return render_template("detail.html")
@@ -17,20 +14,18 @@ def index():
 @app.route("/info", methods=["POST"])
 def ripple_post():
     ripple_receive = request.form['ripple_give']
-
+    print(ripple_receive)
     doc = {
-        'ripple':ripple_receive,
+        'ripple': ripple_receive,
     }
     db.info.insert_one(doc)
 
     return jsonify({'msg': '작성 완료!'})
 
 
-# @app.route("/info", methods=["GET"])
-# def ripple_post():
-#     info_list = list(db.info.find({},{'_id':False}))
-#     return jsonify({'info':info_list})
-
+@app.route('/info', methods=["GET"])
+def index():
+    return render_template("detail.html")
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
