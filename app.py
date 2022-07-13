8b552006eba787dd0e9974a047b82673812fcd43
gwonyeong <qwe980121@naver.com>
2022-07-13 14:10:40 +0900

    게시물의 디자인 완료, 로그인 시 게시물을 작설 할 수 있게 변경

--- app.py
+++ app.py
@@ -24,22 +24,32 @@ def main():
 
 @app.route("/", methods=["POST"])
 def insert_contents_post():
-    try:
-        idx = request.form['index']
-    except:
-        idx = 0
-    artist_receive = request.form['artist_give']
-    title_receive = request.form['title_give']
-    desc_receive = request.form['desc_give']
-    url_receive = request.form['url_give']
-    doc = {'index': idx,
-           'artist': artist_receive,
-           'title': title_receive,
-           'desc': desc_receive,
-           'url': url_receive}
-    db.contents.insert_one(doc)
+    token_receive = request.cookies.get('mytoken')
 
-    return jsonify({'msg': '데이터 POST'})
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])#{'id': 'gwonyeong', 'exp': 1657768562}
+                                                                            # {'id': 'gwonyeong', 'pw': 'eca38cd8f32bd60d105845c50acc190bbf0657df89253d3bf18438463f701d0d'}
+        print(payload)
+        user_id = db.users.find_one({"id": payload["id"]},{'_id': False})
+        print(user_id)
+        idx_list = list(db.contents.find({}, {'_id': False}))
+        idx = len(idx_list) + 1
+
+        artist_receive = request.form['artist_give']
+        title_receive = request.form['title_give']
+        desc_receive = request.form['desc_give']
+        url_receive = request.form['url_give']
+        doc = {'index': idx,
+               'artist': artist_receive,
+               'title': title_receive,
+               'desc': desc_receive,
+               'url': url_receive,
+               'id': user_id['id']}
+        db.contents.insert_one(doc)
+
+        return jsonify({'msg': '공유되었습니다.'})
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return render_template('login.html',msg = '로그인이 필요합니다.')
 
 
 @app.route("/con", methods=["GET"])
@@ -58,8 +68,8 @@ def signup():
     return render_template('signup.html')
 
 
-@app.route('/detail')
-def detail():
+@app.route('/detail?<index>')
+def detail(index):
     return render_template('detail.html')
 
 
@@ -121,6 +131,7 @@ def sign_in():
             'id': username_receive,
             'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
         }
+
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
 
         return jsonify({'result': 'success', 'token': token})
